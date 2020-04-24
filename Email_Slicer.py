d207742800a38860e5e8b6245e2efa1c30064739
ShushiRoy <31969203+ShushiRoy@users.noreply.github.com>
2020-04-24 18:21:56 -0400

    Update Email_Slicer.py
    Allows user to choose to display list of all entries in the table or enter a new email address to add into the table.

--- Email_Slicer.py
+++ Email_Slicer.py
@@ -9,23 +9,34 @@ try:
 
 
     #User Input for an email
-    email= str(input("Please enter your email address: "))
+    user= str(input("Please state if you want to enter an email (type 'email') or see the entire list (type 'list'): "))
+
+    if user=='list':
+        cursor.execute("select * from information")
+        result = cursor.fetchall()
+        for row in result:
+            print("Username =", row[0], )
+            print("Domain =", row[1], "\n")
+    elif user == 'email':        
+        email= str(input("Please enter your email address: "))
 
 
-    #Check to see if the user input is a valid email address
-    if "@" in email:
-        username=email.split("@")[0]
-        domain=email.split("@")[1]
+        #Check to see if the user input is a valid email address
+        if "@" in email:
+            username=email.split("@")[0]
+            domain=email.split("@")[1]
 
 
-        #Insert Data into the PostgreSQL Table
-        cursor.execute("insert into information values ('" + username + "','" + domain + "')")
-        connection.commit()
-        print("Data has been stored")
+            #Insert Data into the PostgreSQL Table
+            cursor.execute("insert into information values ('" + username + "','" + domain + "')")
+            connection.commit()
+            print("Data has been stored")
 
-    #If the user input is not a valud email address, show user message saying so
-    elif "@" not in email:
-        print("Not a valid email")
+        #If the user input is not a valud email address, show user message saying so
+        elif "@" not in email:
+            print("Not a valid email")
+    else:
+        print('Enter a valid input')
 
         
 #Handles for any error that comes from connecting to the database
